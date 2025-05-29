6703abd95f57aea3f4d255e442fb63913f313f73
acostanzo <mail@acostanzo.com>
2025-05-29 15:14:12 -0400

    Add environment validation to release prep script

---
+++ release-prep/lib/environment_validator.rb
@@ -0,0 +1,32 @@
+class EnvironmentValidator
+  REQUIRED_VARIABLES = [
+    "ASANA_PAT",
+    "ASANA_PROJECT_ID",
+    "ASANA_SECTION_ID",
+    "ATLASSIAN_API_TOKEN",
+    "ATLASSIAN_EMAIL",
+    "ATLASSIAN_URL",
+    "CONFLUENCE_RELEASES_SPACE_KEY",
+    "CONFLUENCE_RELEASES_VERSIONS_PARENT_PAGE_ID",
+    "GITHUB_ORG",
+    "GITHUB_PAT",
+    "GITHUB_REPO"
+  ].freeze
+
+  def self.validate!
+    new.validate!
+  end
+
+  def validate!
+    missing_vars = REQUIRED_VARIABLES.select { |var| ENV[var].nil? || ENV[var].empty? }
+
+    if missing_vars.any?
+      raise EnvironmentError, <<~ERROR
+        The following required environment variables are not set:
+        #{missing_vars.map { |var| "  - #{var}" }.join("\n")}
+      ERROR
+    end
+  end
+
+  class EnvironmentError < StandardError; end
+end 
