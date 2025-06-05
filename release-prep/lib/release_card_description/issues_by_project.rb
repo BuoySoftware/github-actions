6abc27ec038ff624435b52efabbc5fbb91978132
acostanzo <mail@acostanzo.com>
2025-06-05 17:27:45 -0400

    Group PRs under issues by project

--- release-prep/lib/release_card_description/issues_by_project.rb
+++ release-prep/lib/release_card_description/issues_by_project.rb
@@ -12,11 +12,41 @@ module ReleaseCardDescription
           <<~MARKDOWN
             h3. #{project.name}
             #{issues.map do |issue|
-              " - #{issue.key}"
+              " # #{issue.key}\n" +
+              pull_requests_for_issue(issue).map do |pull_request|
+                " *# [##{pull_request.number}: #{pull_request.title}|#{pull_request.html_url}]"
+              end.join("\n")
             end.join("\n")}
           MARKDOWN
         end.join("\n\n")}
       MARKDOWN
+    end
+
+    private
+
+    def pull_requests_for_issue(issue)
+      pull_requests_by_issue.find do |group|
+        group[:issue] == issue
+      end[:pull_requests]
+    end
+
+    def pull_requests_by_issue
+      release.jira_assets.issues.map do |issue|
+        {
+          issue:,
+          pull_requests: release.github_assets.pull_requests.select do |pull_request|
+            commit_messages = release.github_assets.commits_by_pull_request.find do |group|
+              group[:pull_request] == pull_request
+            end[:commits].map(&:commit).map(&:message).join("\n")
+
+            [
+              pull_request.title.include?(issue.key),
+              pull_request.body&.include?(issue.key),
+              commit_messages.include?(issue.key),
+            ].any?
+          end,
+        }
+      end
     end
   end
 end 
